4ec013841704c746de9dd315105417610ace4b6b
gauharbauyrzhankyzy <bauyrzhankyzy.g@husky.neu.edu>
2017-07-25 17:17:30 -0400

    before everything broke

--- public/assignment/views/widget/controllers/widget-new.controller.client.js
+++ public/assignment/views/widget/controllers/widget-new.controller.client.js
@@ -11,9 +11,7 @@
         var widgets = [
             {"_id": "123", "widgetType": "HEADING", "pageId": "321", "size": 2, "text": "GIZMODO"},
             {"_id": "234", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
-            {
-                "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%",
-                "url": "http://lorempixel.com/400/200/"
+            {"_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%", "url": "http://lorempixel.com/400/200/"
             },
             {"_id": "456", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"},
             {"_id": "567", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
